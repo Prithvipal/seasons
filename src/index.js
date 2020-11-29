3815d8354e2ac7355aaff6965bde3a2fcbcb13a1
Prithvipal Singh <prithvirathore99@gmail.com>
2020-11-29 19:23:03 +0530

    function to class with geolocation api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
  
-const App = ()=>{
 
-    return (
-        <div>Heeeyyy</div>
-    )
-};
+class App extends React.Component{
+    render(){
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
+        return <div>Heeeyyy</div>
+    }
+}
 
 ReactDOM.render(<App/>, document.querySelector("#root"))
