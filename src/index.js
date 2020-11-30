6e05e9562f20df21bef7b524d0788bb183fe6228
Prithvipal Singh <prithvirathore99@gmail.com>
2020-11-30 09:07:32 +0530

    conditional error messages

--- src/index.js
+++ src/index.js
@@ -5,18 +5,24 @@ import ReactDOM from 'react-dom'
 class App extends React.Component{
     constructor(props){
         super(props);
-        this.state = { lat: null };
+        this.state = { lat: null, errMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({lat: position.coords.latitude})
             },
-            err => console.log(err)
+            err => {
+                this.setState({errMessage: err.message})
+            }
         );
     }
     render(){
-       
-        return <div>Latitude: {this.state.lat} </div>
+        if (this.state.lat){
+            return <div>Latitude: {this.state.lat} </div> 
+        } else if (this.state.errMessage){
+            return <div>Error: {this.state.errMessage} </div> 
+        }
+        return <div>Loading...</div>
     }
 }
 
